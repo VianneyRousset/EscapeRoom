dabb7830171f572a85c89251dd52c9869454f270
VianneyRousset <6491145+VianneyRousset@users.noreply.github.com>
2018-07-14 11:46:16 +0200

    new StringQueue

--- arduinos/libs/array/array.cpp
+++ arduinos/libs/array/array.cpp
@@ -32,3 +32,68 @@ int queue_pull(Queue_t* q, void* data)
 	return q->n;
 }
 
+StringQueue::StringQueue(unsigned short bufferSize, char* buffer)
+{
+	this->p = 0;
+	this->n = 0;
+	this->N = bufferSize;
+	this->buffer = buffer;
+}
+
+int	StringQueue::push(const char* str)
+{
+	const unsigned short end = (this->p+this->n) % this->N;
+	const unsigned short len = strlen(str)+1;
+	
+	// can be placed at the end
+	if (end+len <= this->N) {
+		memcpy(this->buffer+end, str, len);
+		this->n += len;
+		return this->N - this->n;
+	}
+
+	// has to be place at beginning
+	if (len > p) { // no enough place at beginning
+		return -1;
+	}
+
+	memset(this->buffer+end, '\0', this->N-end);
+	this->n += this->N-end;
+	memcpy(this->buffer, str, len);
+	this->n += len;
+	return this->N - this->n;
+}
+
+const char* StringQueue::get(void)
+{
+	if (this->n == 0)
+		return NULL;
+	return this->buffer + p;
+}
+
+int StringQueue::remove(void)
+{
+	unsigned short len = strlen(this->buffer+this->p)+1;
+
+	if (this->n == 0) { // trying to remove element in empty buffer
+		return -1;
+	}
+
+	if (this->buffer[p+len] == '\0') {
+		len = this->N - this->p;
+		this->p = 0;
+	} else {
+		this->p += len;
+	}
+
+	if (len > n) {
+		this->n = 0;
+	} else {
+		this->n -= len;
+	}
+
+	if (this->n == 0)
+		this->p = 0;
+
+	return this->N - this->n;
+}
--- arduinos/libs/array/array.hpp
+++ arduinos/libs/array/array.hpp
@@ -3,6 +3,8 @@
 
 // fifo
 typedef struct {
+	// p: first element ptr, n: nb of elements, s: sizeof element in bytes
+	// N: buffers total size in bytes
 	unsigned short p, n, s, N;
 	void* buffer;
 } Queue_t;
@@ -17,5 +19,36 @@ int queue_push(Queue_t* queue, const void* data);
 // pull data from queue and return the number of remaining elements in the queue
 // return -1 if the queue was empty before reading
 int queue_pull(Queue_t* queue, void* data);
+
+class StringQueue {
+
+	private:
+
+	public:
+		// pointer to first str
+		unsigned short p;
+
+		// number of characters
+		unsigned short n;
+
+		// size of the buffer in bytes 
+		unsigned short N;
+
+		char* buffer;
+	
+		// buffer size in byte
+		StringQueue(unsigned short bufferSize, char* buffer);
+
+		// push str to queue. Return nb of free buffer space in bytes.
+		// In case of buffer overflow, str is not copied and return -1 
+		int	push(const char* str);
+
+		// get str ptr to str first string.
+		const char* get(void);
+
+		// remove first element. Return nb of free buffer space in bytes.
+		// return -1 if the buffer was already empty before removing.
+		int remove(void);
+};
 
 #endif
